54214953e03e868f5d893c8c43452955ef1e9740
Brian Eng <brian@brianeng.com>
2020-05-18 20:36:44 -0500

    initial commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sinatra"                                                                     #
+require "sinatra/reloader" if development?                                            #
+require "sequel"                                                                      #
+require "logger"                                                                      #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                              #
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
+def view(template); erb template.to_sym; end                                          #
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'           #
+before { puts "Parameters: #{params}" }                                               #
+after { puts; }                                                                       #
+#######################################################################################
+
+events_table = DB.from(:events)
+rsvps_table = DB.from(:rsvps)
+
+get "/" do
+  @events = events_table.all
+  puts @events.inspect
+  view "events"
+end
