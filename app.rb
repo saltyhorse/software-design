b4aa9d8f44ce8122e4a55eabdaaaa5bee40298f6
saltyhorse <stacyxma@gmail.com>
2020-05-21 02:26:40 +0000

    post class changes

--- app.rb
+++ app.rb
@@ -20,3 +20,29 @@ get "/" do
   puts @events.inspect
   view "events"
 end
+
+get "/events/:id" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    puts @event
+    view "event"
+end
+
+get "/events/:id/rsvps/new" do
+    @event = events_table.where(:id => params["id"]).to_a[0]
+    @rsvps = rsvps_table.where(:id => params["id"]).to_a[0]
+
+    @count = rsvps_table.where(:id => params["id"], :going => true).count
+    puts @event.inspect
+    puts @rsvps.inspect
+    view "new_rsvp"
+end
+
+get "/events/:id/rsvps/create" do
+    params.inspect
+    rsvps_table.insert(:event_id => params["id"],
+                       :going => params["going"],
+                        :name => params["name"],
+                        :email => params["email"],
+                        :comments => params["comments"])
+    view "create_rsvp"
+end
